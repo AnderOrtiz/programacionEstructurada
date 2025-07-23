9203c4d7b5f8f44850f8e06003c496d26318028b
ander-ortiz <ortizone620@gmail.com>
2025-07-23 09:13:19 -0600

    semana 1

---
+++ semana1/index.js
@@ -0,0 +1,28 @@
+let name = "Elizabeth";
+let alias = 'Eli';
+let email = 'a@a.com';
+console.log(`Hola ${ name } como estas?`);
+
+let age =  37; // entero 
+let altura = 12.50; //flotante
+
+//Tipo de dato booleano
+let isStudent = true;
+let isTeacher = false;
+
+//Tipo de dato undefined
+let valorUndefined;
+console.log(valorUndefined)
+
+//Tipo de dato Null
+let nullValue = null;
+
+
+//tipo de dato Symbol
+let muSymbol = Symbol('mySimbol');
+console.log(muSymbol)
+
+// tipo de dato BigInt
+let myBigInt = BigInt(2367890853234567890874345678987543478987345678)
+let myBig = 2367890853234567890874345678987543478987345678n
+console.log(myBigInt, myBig)
---
+++ semana1/variables.js
@@ -0,0 +1,12 @@
+// console.log(helloWorld);
+// var helloWorld = "Hola mundo"
+// console.log(helloWorld);
+
+// variables let
+// console.log(helloWorld);
+
+
+let helloWorld = 'Hola mundo'
+helloWorld = 'esto es un nuevo mensaje'
+console.log(helloWorld);
+
